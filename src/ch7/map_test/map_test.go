7558b488517ea98e6b69c5270d1865cf2c40ade3
lee <leejiliang@126.com>
2024-05-13 23:12:45 +0800

    feat: 测试map, 判断key是否存在时, 使用的方法比较特殊, 需要注意

---
+++ src/ch7/map_test/map_test.go
@@ -0,0 +1,27 @@
+package map_test
+
+import "testing"
+
+func TestMapInit(t *testing.T) {
+	m := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
+	t.Log(len(m), m["A"])
+
+	m2 := map[string]int{}
+	m2["A"] = 4
+	t.Log(len(m2), m2["A"])
+
+	m3 := make(map[string]int, 10) // cap 10
+	m3["A"] = 5
+	t.Logf("m3 len:%d", len(m3))
+	//t.Logf("m3 len:%d", cap(m3))// 无法通过cap()获取cap
+}
+
+func TestMapExistsKey(t *testing.T) {
+	map1 := map[int]string{1: "a", 2: "b", 3: "c"}
+
+	if v, ok := map1[4]; ok {
+		t.Logf("key:%s exists:%t", v, ok)
+	} else {
+		t.Logf("key:%s not exists:%t", v, ok)
+	}
+}
