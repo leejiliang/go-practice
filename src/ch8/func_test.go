263e7ed6b1e81282565892ae1c6aa95144756203
lee <leejiliang@126.com>
2024-05-15 22:50:21 +0800

    feat: go 里面函数支持返回多个值, 函数的参数和返回值都可以是函数, 巧妙的构造实现带计时功能的函数

---
+++ src/ch8/func_test.go
@@ -0,0 +1,46 @@
+package ch8
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+/*
+*
+返回多个值的函数
+*/
+func returnMultiVal() (int, int) {
+	return rand.Intn(10), 7
+}
+
+func TestFn(t *testing.T) {
+	a, b := returnMultiVal()
+	t.Log(a, b)
+
+	// 构造带计时功能的函数
+	tsFG := timeSpent(slowFunc)
+	// 执行带计时功能的函数
+	t.Log(tsFG(10))
+}
+
+/*
+*
+参数是一个函数, 返回值也是一个函数
+*/
+func timeSpent(inner1 func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now() // 获取开始时间
+		ret := inner1(n)    //执行参数函数
+		// 计算耗时并打印
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		// 返回执行结果
+		return ret
+	}
+}
+
+func slowFunc(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
